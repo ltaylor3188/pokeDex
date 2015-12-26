9e6fa3890b2e0161acdb5b45c46b2856a6df9f42
Les Taylor <lestaylor@Less-MacBook-Pro.local>
2015-12-26 06:53:38 -0600

    update

---
+++ pokeDex/pokeMon.swift
@@ -0,0 +1,30 @@
+//
+//  pokeMon.swift
+//  pokeDex
+//
+//  Created by Les Taylor on 12/25/15.
+//  Copyright © 2015 Les Taylor. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
